380e2a8ff0c038a78171602e64439f7e8297cc9c
hieuneko <83810971+hieuneko@users.noreply.github.com>
2022-11-10 21:02:11 +0700

    Done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Happy-Birthday
+# Happy-birthday
+This page is dedicated to the person I like. So don't get any picture in here and pulic to internet
---
+++ happy_birthday.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $(".candles").click(function() {
+
+        $(".flame").animate({ "opacity": 0 }, "fast");
+        $(".flame2").animate({ "opacity": 0 }, "fast");
+        $(".flame3").animate({ "opacity": 0 }, "fast");
+        $(".text").animate({ "top": -200, "opacity": 1, "width": 380 }, "fast");
+        $(".text").delay(1000).fadeIn().animate({ "top": -200, "opacity": 0, "width": 380 }, "fast");
+        $(".text1").delay(2000).animate({ "top": -200, "opacity": 1, "width": 380 }, "medium");
+        $(".text1").delay(2000).fadeIn().animate({ "top": -200, "opacity": 0, "width": 380 }, "fast");
+        $(".text2").delay(5000).animate({ "top": -200, "opacity": 1, "width": 380 }, "fast");
+        $(".text2").delay(2000).fadeIn().animate({ "top": -200, "opacity": 0, "width": 380 }, "fast");
+        $(".text3").delay(8000).animate({ "top": -200, "opacity": 1, "width": 380 }, "fast");
+        $(".text3").delay(3000).fadeIn().animate({ "top": -200, "opacity": 0, "width": 380 }, "fast");
+        setTimeout(function() {
+            $(".modal").css("display", "block");
+        }, 13000);
+
+    });
+    $(".next").click(function() {
+        $(".birthday-cake").animate({ "opacity": 0 }, "fast");
+        $("body").css("overflow", "hidden");
+        $(".star-sky").css("display", "block");
+        setTimeout(function() {
+            $("#firework").css("display", "block");
+        }, 200);
+    });
+});
